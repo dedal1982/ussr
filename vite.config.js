304e64b7f66c83678a4c7db9fbf50b9c5d45b172
Al Dm <d44.a1@yandex.ru>
2025-10-30 11:53:32 +0300

    build(deps): update package-lock.json, package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import viteCompression from "vite-plugin-compression";
-// Импортируем плагин PurgeCSS для удаления неиспользуемых стилей
 import vitePurgeCss from "vite-plugin-purgecss";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 export default defineConfig({
   plugins: [
     react(),
     viteCompression(),
-    // Добавляем PurgeCSS
     vitePurgeCss({
       content: ["./index.html", "./src/**/*.jsx", "./src/**/*.tsx"],
       safelist: [],
+    }),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          // Можно вставлять дополнительные теги или переменные
+        },
+        tags: [
+          {
+            tag: "link",
+            attrs: {
+              rel: "preload",
+              as: "style",
+              href: "/assets/index-ByPdVx4C.css",
+            },
+          },
+        ],
+      },
     }),
   ],
   build: {
