29a558848b491b77c1af6f67a6422e73fd3ef950
Al Dm <d44.a1@yandex.ru>
2025-10-30 11:12:36 +0300

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import viteCompression from "vite-plugin-compression";
+// Импортируем плагин PurgeCSS для удаления неиспользуемых стилей
+import vitePurgeCss from "vite-plugin-purgecss";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteCompression(),
+    // Добавляем PurgeCSS
+    vitePurgeCss({
+      content: ["./index.html", "./src/**/*.jsx", "./src/**/*.tsx"],
+      safelist: [],
+    }),
+  ],
+  build: {
+    minify: "esbuild",
+    cssCodeSplit: true,
+  },
+});
