2fdd6b522c471e44ea740f6bb1ddc759226269a9
Al Dm <d44.a1@yandex.ru>
2025-11-24 01:00:10 +0300

    update 4 files and delete 1 file

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
+import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ErrorBoundaryWrapper } from "@components/ErrorBoundary/ErrorBoundary.jsx";
-import "./assets/fonts/fonts.css";
-import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
